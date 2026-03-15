bc5563a8af9f252d57f61bdffcf45aac7e3ee557
GrasPanda <Lucasus822@gmail.com>
2026-03-15 14:16:04 +0100

    composable useAuth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/icon',
-    '@nuxt/fonts',
-    '@nuxtjs/i18n',
-    '@nuxtjs/tailwindcss'
-  ]
-})
+  modules: ["@nuxt/icon", "@nuxt/fonts", "@nuxtjs/i18n", "@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    public: {
+      apiUrl: "htpp://localhost:3001",
+    },
+  },
+  i18n: {
+    locales: [{ code: "fr", language: "fr-FR" }],
+    defaultLocale: "fr",
+  },
+});
